a623159f95575566b8d2a25b6f616fa05e8a7681
SimonLowther <simon@jlobservatory.com>
2021-03-24 19:51:34 +1300

    create loadsheet page elements

--- lib/loadsheet.dart
+++ lib/loadsheet.dart
@@ -1,3 +1,4 @@
+import 'package:dummy_load/main.dart';
 import 'package:flutter/material.dart';
 
 class Loadsheet extends StatefulWidget {
@@ -8,17 +9,35 @@ class Loadsheet extends StatefulWidget {
 class _LoadsheetState extends State<Loadsheet> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context, rootNavigator: true).pop();
-            },
-            child: Text('Modify Load'),
-          ),
-        ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context, rootNavigator: true).pop();
+                  },
+                  child: Text('Modify Load'),
+                ),
+                ElevatedButton(
+                    onPressed: () {}, child: Text('  Copy to \n Clipboard')),
+                ElevatedButton(
+                    onPressed: () {}, child: Text('Print Loadsheet')),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 40, 0, 12),
+              child: Text(MyApp.loadsheet.toString()),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+class Alerts {}
